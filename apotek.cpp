b5bf37e2c0eb38e0b16cdf614708fcbe335bd375
Fadhil Akbar <fadhilakbar57894@gmail.com>
2026-04-21 09:30:13 +0700

    fitur awal (main)

--- apotek.cpp
+++ apotek.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
+void pilihMenu() {
+    cout << "===== Kasir Apotek =====" << endl;
+    cout << "1. Obat Pusing" << endl;
+    cout << "2. Obat Batuk Pilek" << endl;
+    cout << "3. Obat Lambung" << endl;
+    cout << "Mau nyari obat apa kak?";    
+}
+
+int totalHarga() {
+    
+}
+
+
 int main() {
-    cout << "Hello";
+    int pilihObat;
+
+
+
+
 }
